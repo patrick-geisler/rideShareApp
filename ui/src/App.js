a993c4ed0092f5475e539c563cfb9978913c976d
Matt Matuszak <mattmatuszak@northwesternmutual.com>
2017-06-12 15:14:05 -0500

    added login page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import LoginPage from './components/LoginPage';
-import {BrowserRouter as Router, Route , Redirect} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import Content from './components/Content'
-const App = () => {
 
-  return(
-    <Router>
-        <div> 
-          <Route exact path='/' component={LoginPage}/> 
-          <Route path='/rideshare' component={Content}/>  
-           
-        </div>
-    </Router>
-  )
+class App extends React.Component {
+
+
+    state = {
+        user: null
+    }
+
+    loginAction = (username) => {
+        this.setState({user: {username}})
+    }
+
+    render() {
+
+        let route = <Redirect to='/rideshare' />
+        if (this.state.user === null) {
+            route = <LoginPage loginAction={this.loginAction} />
+        }
+
+        return (
+            <Router>
+                <div>
+                    {route}
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App;
--- ui/src/components/LoginPage.js
+++ ui/src/components/LoginPage.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class LoginPage extends React.Component {
+
+    state = {
+        login: ''
+    }
+
+    loginChange = (event) => {
+        this.setState({login: event.target.value})
+    }
+
+    submit = (event) => {
+        event.preventDefault();
+        console.log(this.state.login)
+        this.props.loginAction(this.state.login)
+    }
 
 
+    render() {
+        return (
+            <div className='gridContainer'>
+                <div className='row'>
+                    <div className='small-3 small-centered columns'>
+                        <h1>Please Login</h1>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='small-3 small-centered columns'>
+                        <form onSubmit={this.submit}>
+                            <input type="text" onChange={this.loginChange} name="LoginPage" placeholder="enter username"/>
+                            <button type="submit" className="tiny">Sign-in</button>
+                        </form>
+                    </div>
 
-const LoginPage = () => {
-  return(
-    <div>LoginPage
-    </div>
-  )
+                </div>
+            </div>
+        )
+    }
 }
 
 export default LoginPage;
