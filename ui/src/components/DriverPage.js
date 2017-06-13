dc0a8dba7c058af2e2ae051b903250ede124b90d
KOSINSKI, COLLIN <collinkosinski@northwesternmutual.com>
2017-06-13 09:57:47 -0500

    aligned driverPage form

--- ui/src/components/DriverPage.js
+++ ui/src/components/DriverPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '.././App.css';
 
 
 
@@ -9,39 +10,35 @@ const DriverPage = () => {
   return(
     <form>
         <div className="row">
-            <div className="small-12 small-centered large-6 large-centered columns">
+            <div className="small-10 small-centered large-8 large-centered columns">
                 <h1>Please Fill Out Form to Create a Trip</h1>     
             </div>
         </div>
         <div className="row">
-            <div className="large-6 large-centered columns">
+            <div className="small-12 small-centered large-6 large-centered columns">
                 <label>Driver Name</label>
                 <input type="text" placeholder="Please enter driver name"/>
             </div>
         </div>
         <div className="row">
-            <div className="large-6 large-centered columns">
+            <div className="large-3 large-centered inline-block columns">
                 <label>Date:</label>
-                <input type="date" name="date"/>
+                <input placeholder= "Please select date" type="date"/>
             </div>
-        </div>
-        <div className="row">
-            <div className="large-6 large-centered columns">
+        <   div className="large-3 large-centered inline-block columns">
                 <label>Time:</label>
-                <input type="time" name="time"/>
+                <input placeholder="Please select time" type="time"/>
             </div>
         </div>
         <div className="row">
-            <div className="large-6 large-centered columns">
+            <div className="large-3 large-centered inline-block columns">
                 <label>Leaving From</label>
-                <input id="franklin" name="franklin" type="radio" value="franklin"/>
+                <input id="franklin" name="leavingfrom" type="radio" value="franklin"/>
                 <label for="franklin">Franklin</label>
-                <input id="downtown" name="downtown" type="radio" value="downtown"/>
+                <input id="downtown" name="leavingfrom" type="radio" value="downtown"/>
                 <label for="downtown">Downtown</label>
             </div>
-        </div>
-        <div className="row">
-            <div className="large-6 large-centered columns">
+            <div className="large-3 large-centered inline-block columns">
                 <label>Number of Passengers</label>
                 <select>
                 <option value="1">
@@ -60,7 +57,7 @@ const DriverPage = () => {
             </div>
         </div>
         <div className="row">
-            <div className="large-6 large-centered columns">
+            <div className="large-3 large-centered columns">
                 <button>Create Trip</button>
             </div>
         </div>
