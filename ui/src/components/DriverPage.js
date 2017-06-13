146fa8d58af8934508bb21966393adda1b79f96c
patrick-geisler <patrickgeisler@northwesternmutual.com>
2017-06-13 10:48:43 -0500

    Working trip

--- ui/src/components/DriverPage.js
+++ ui/src/components/DriverPage.js
@@ -1,68 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '.././App.css';
+import axios from 'axios';
+import {BrowserRouter as Router, Route , Redirect, Link} from 'react-router-dom'
 
 
 
 
-const DriverPage = () => {
+class DriverPage extends React.Component{
+  state={
+    name: "",
+    date: "",
+    time: "",
+    numPass: "",
+    leavingFrom: "",
+    submitted: false
+  }
+  handleSubmit = ( event )=>{
+    event.preventDefault()
+    axios.post('/api/trips', this.state)
+    .then(response => {
+      console.log(response.data);
+      this.setState({submitted: true})
+    })
 
+  }
+  stateChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+render(){
+  if (this.state.submitted === true){
+    return <Redirect to="/rideshare"/>
+  }
   return(
-    <form>
-        <div className="row">
-            <div className="small-10 small-centered large-8 large-centered columns">
-                <h1>Please Fill Out Form to Create a Trip</h1>     
-            </div>
-        </div>
-        <div className="row">
-            <div className="small-12 small-centered large-6 large-centered columns">
-                <label>Driver Name</label>
-                <input type="text" placeholder="Please enter driver name"/>
-            </div>
-        </div>
-        <div className="row">
-            <div className="large-3 large-centered inline-block columns">
-                <label>Date:</label>
-                <input placeholder= "Please select date" type="date"/>
-            </div>
-        <   div className="large-3 large-centered inline-block columns">
-                <label>Time:</label>
-                <input placeholder="Please select time" type="time"/>
-            </div>
-        </div>
-        <div className="row">
-            <div className="large-3 large-centered inline-block columns">
-                <label>Leaving From</label>
-                <input id="franklin" name="leavingfrom" type="radio" value="franklin"/>
-                <label for="franklin">Franklin</label>
-                <input id="downtown" name="leavingfrom" type="radio" value="downtown"/>
-                <label for="downtown">Downtown</label>
-            </div>
-            <div className="large-3 large-centered inline-block columns">
-                <label>Number of Passengers</label>
-                <select>
-                <option value="1">
-                    1
-                </option>
-                <option value="2">
-                    2
-                </option>
-                <option value="3">
-                    3
-                </option>
-                <option value="4">
-                    4
-                </option>
-            </select>
-            </div>
-        </div>
-        <div className="row">
-            <div className="large-3 large-centered columns">
+      <form onSubmit={this.handleSubmit}>
+          <div className="row">
+              <div className="small-10 small-centered large-8 large-centered columns">
+                  <h1>Please Fill Out Form to Create a Trip</h1>
+              </div>
+          </div>
+          <div className="row">
+              <div className="small-12 small-centered large-6 large-centered columns">
+                  <label>Driver Name</label>
+                  <input name="name" type="text" onChange={this.stateChange} placeholder="Please enter driver name"/>
+              </div>
+          </div>
+          <div className="row">
+              <div className="large-3 large-centered inline-block columns">
+                  <label>Date:</label>
+                  <input placeholder= "Please select date" name="date" onChange={this.stateChange} type="date"/>
+              </div>
+          <div className="large-3 large-centered inline-block columns">
+                  <label>Time:</label>
+                  <input placeholder="Please select time" name="time" onChange={this.stateChange} type="time"/>
+              </div>
+          </div>
+          <div className="row">
+              <div className="large-3 large-centered inline-block columns">
+                  <label>Leaving From</label>
+                  <input id="franklin" name="leavingFrom" type="radio" value="franklin"
+                      onChange={this.stateChange} checked={this.state.leavingFrom === 'franklin'} />
+                  <label for="franklin">Franklin</label>
+                  <input id="downtown" name="leavingFrom" type="radio" value="downtown"
+                      onChange={this.stateChange} checked={this.state.leavingFrom === 'downtown'}/>
+                  <label for="downtown">Downtown</label>
+              </div>
+              <div className="large-3 large-centered inline-block columns">
+                  <label>Number of Passengers</label>
+                  <select name="numPass" onChange={this.stateChange}>
+                  <option value="1">
+                      1
+                  </option>
+                  <option value="2">
+                      2
+                  </option>
+                  <option value="3">
+                      3
+                  </option>
+                  <option value="4">
+                      4
+                  </option>
+              </select>
+              </div>
+          </div>
+          <div className="row">
+              <div className="large-3 large-centered columns">
                 <button>Create Trip</button>
-            </div>
-        </div>
-    </form>
-  )
+              </div>
+          </div>
+      </form>
+    )
+  }
 }
 
 
---
+++ ui/src/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bp = require('body-parser')
+
+const app = express();
+
+app.use(bp.json())
+app.use(bp.urlencoded({extended: true}))
+
+app.get('/api/ping', (request, response) => {
+    response.json({message: 'hello'})
+})
+
+const trips = [
+    {id: 123, name: "Mark", numPass: 10, plateNum: "267-JKL", date: "6/10/2017" ,time: 800 ,leavingFrom: "Franklin" }
+    , {id: 456, name: "Patrick", numPass: 4, plateNum: "849-YUI", date: "6/10/2017" ,time: 900 ,leavingFrom: "Franklin" }
+    , {id: 789, name: "Jobben", numPass: 1, plateNum: "LV2RIDE", date: "6/10/2017" ,time: 830 ,leavingFrom: "Downtown" }
+];
+
+app.get('/api/trips', (request, response) => {
+    // console.log(request.query)
+    // switch(request.query.filter) {
+    //         const activeTodos = trips.filter(trips => numPass)
+    //         response.json(activeTodos);
+    //         break;
+    //     default: response.json(trips);
+    // }
+});
+
+app.post('/api/trips', (request, response) => {
+    console.log(request.body);
+    const newTrip = {
+        id: Math.floor(Math.random()*10000)
+        , name: request.body.name
+        , numPass: request.body.numPass
+        , plateNum: request.body.plateNum
+        , date: request.body.date
+        , time: request.body.time
+        , leavingFrom: request.body.leavingFrom
+    }
+    trips.push(newTrip)
+    response.json(newTrip)
+})
+
+
+app.listen(6500, () => console.log('started'))
